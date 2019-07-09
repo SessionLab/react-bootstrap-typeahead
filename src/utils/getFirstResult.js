9a8fdea8de456c3c1aff017c7b5a667bb293ba0d
Roman <roma.khudyakov@gmail.com>
2019-07-09 21:48:27 +0300

    feat(highlight-first) suppport highlighting and selecting first item

---
+++ src/utils/getFirstResult.js
@@ -0,0 +1,6 @@
+
+function getFirstResult({highlightFirstResult, position, activeIndex}) {
+  return highlightFirstResult && position === 0 && activeIndex === -1;
+}
+
+export default getFirstResult;
