1480bd909f2eadc8bcc035601b1e03c61d08040d
Roman <roma.khudyakov@gmail.com>
2019-07-12 00:57:36 +0300

    remove non dom attributes from input

--- src/containers/hintContainer.js
+++ src/containers/hintContainer.js
@@ -52,6 +52,8 @@ function hintContainer(Input) {
         onAdd,
         selectHintOnEnter,
         highlightFirstResult,
+        isMenuShown,
+        minLength,
         ...props
       } = this.props;
 
